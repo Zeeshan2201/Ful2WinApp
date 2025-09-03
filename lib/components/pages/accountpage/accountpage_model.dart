e8a49bd2e6f5b33d3c0835ed38d857d48b357f0f
gitP70hub <pranalikhonde93@gmail.com>
2025-09-03 11:35:44 +0530

    accountpage widget added

--- lib/components/pages/accountpage/accountpage_model.dart
+++ lib/components/pages/accountpage/accountpage_model.dart
@@ -1,17 +1,24 @@
 import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_animations.dart';
 import '/flutter_flow/flutter_flow_theme.dart';
 import '/flutter_flow/flutter_flow_util.dart';
 import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:math';
 import 'dart:ui';
 import '/index.dart';
 import 'accountpage_widget.dart' show AccountpageWidget;
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:just_audio/just_audio.dart';
 import 'package:provider/provider.dart';
 
 class AccountpageModel extends FlutterFlowModel<AccountpageWidget> {
   ///  State fields for stateful widgets in this page.
 
+  AudioPlayer? soundPlayer1;
+  AudioPlayer? soundPlayer2;
   // State field(s) for TextField widget.
   FocusNode? textFieldFocusNode1;
   TextEditingController? textController1;
